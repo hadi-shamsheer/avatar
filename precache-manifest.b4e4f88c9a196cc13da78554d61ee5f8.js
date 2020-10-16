6dc1c2f23211e6f38238252d1800c17a773c3ae5
unknown <hadimshamsheer@gmail.com>
2020-10-16 00:37:37 -0700

    Updates

---
+++ precache-manifest.b4e4f88c9a196cc13da78554d61ee5f8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e0cd674205a97d3f0aeae593566eaeb",
+    "url": "/avatar/index.html"
+  },
+  {
+    "revision": "f7f3cbb6d5631419992b",
+    "url": "/avatar/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "366e4837e01348981219",
+    "url": "/avatar/static/css/main.fc534efe.chunk.css"
+  },
+  {
+    "revision": "f7f3cbb6d5631419992b",
+    "url": "/avatar/static/js/2.e005bfef.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/avatar/static/js/2.e005bfef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "366e4837e01348981219",
+    "url": "/avatar/static/js/main.237fabe1.chunk.js"
+  },
+  {
+    "revision": "5ca4d1ef53b698669da8",
+    "url": "/avatar/static/js/runtime-main.5de3be59.js"
+  }
+]);
